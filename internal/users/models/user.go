6ff12821af3b88d87421e747999be366aca62175
Osezele Orukpe <osezele.o@gmail.com>
2024-06-15 18:23:55 +0100

    Refactor User model to use Bun ORM
    The User model has been refactored to use the Bun ORM instead of raw Go structures. The fields for the User struct are now annotated with Bun ORM specific tags providing details such as the table name, type of primary key, auto incrementation, nullability of fields and default values.

--- internal/users/models/user.go
+++ internal/users/models/user.go
@@ -1,14 +1,19 @@
 package models
 
-import "time"
+import (
+	"github.com/uptrace/bun"
+	"time"
+)
 
 type User struct {
-	ID        int
-	Email     string
-	Password  string
-	Name      string
-	Gender    string
-	Dob       time.Time
-	CreatedAt time.Time
-	UpdatedAt time.Time
+	bun.BaseModel `bun:"table:users"`
+
+	ID        int64     `bun:"id,pk,autoincrement"`
+	Email     string    `bun:"email,notnull"`
+	Password  string    `bun:"password,notnull"`
+	Name      string    `bun:"name,notnull"`
+	Gender    string    `bun:"gender,notnull"`
+	Dob       time.Time `bun:"dob,notnull"`
+	CreatedAt time.Time `bun:"created_at,nullzero,notnull,default:current_timestamp"`
+	UpdatedAt time.Time `bun:"updated_at,nullzero,notnull,default:current_timestamp"`
 }
