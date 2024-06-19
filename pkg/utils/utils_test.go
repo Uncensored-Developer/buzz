b19087e969ba9d357f3f670df71d8ac67ad8b880
Osezele Orukpe <osezele.o@gmail.com>
2024-06-19 17:29:25 +0100

    Add distance calculation functionality
    Implemented a function that calculates the distance between two geographic coordinates using the Haversine formula in the utility package. This function is vital for calculating distances in geographic applications, for instance, in location-based services where distances between locations need to be determined. Additionally, a corresponding test function was created in the utils_test.go file to ensure its correct operation.

--- pkg/utils/utils_test.go
+++ pkg/utils/utils_test.go
@@ -3,6 +3,7 @@ package utils_test
 import (
 	"github.com/Uncensored-Developer/buzz/pkg/utils"
 	"github.com/pkg/errors"
+	"math"
 	"testing"
 )
 
@@ -50,6 +51,61 @@ func TestConvertDatabaseUrlToDSN(t *testing.T) {
 				if dsn != tc.expectedDSN {
 					t.Fatalf("expected DSN '%s' but got '%s'", tc.expectedDSN, dsn)
 				}
+			}
+		})
+	}
+}
+
+func TestDistanceBetween(t *testing.T) {
+	testCases := map[string]struct {
+		lat1, long1  float64
+		lat2, long2  float64
+		expectedDist float64
+	}{
+		"San Francisco to Los Angeles": {
+			lat1:         37.7749,
+			long1:        -122.4194,
+			lat2:         34.0522,
+			long2:        -118.2437,
+			expectedDist: 559.0, // Approximate distance in KM
+		},
+		"New York to Los Angeles": {
+			lat1:         40.7128,
+			long1:        -74.0060,
+			lat2:         34.0522,
+			long2:        -118.2437,
+			expectedDist: 3935.0,
+		},
+		"London to Paris": {
+			lat1:         51.5074,
+			long1:        -0.1278,
+			lat2:         48.8566,
+			long2:        2.3522,
+			expectedDist: 344.0,
+		},
+		"Tokyo to Sydney": {
+			lat1:         35.6895,
+			long1:        139.6917,
+			lat2:         -33.8688,
+			long2:        151.2093,
+			expectedDist: 7826.0,
+		},
+		"UserA to UserB": {
+			lat1:         51.2725887,
+			long1:        0.5026768,
+			lat2:         50.96284649,
+			long2:        -0.12981616,
+			expectedDist: 55.0,
+		},
+	}
+
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := utils.DistanceBetween(tc.lat1, tc.long1, tc.lat2, tc.long2)
+			if math.Abs(got-tc.expectedDist) > 1.0 { // Allowing 1 km error
+				t.Errorf(
+					"DistanceBetween(%f, %f, %f, %f) = %f; want %f",
+					tc.lat1, tc.long1, tc.lat2, tc.long2, got, tc.expectedDist)
 			}
 		})
 	}
